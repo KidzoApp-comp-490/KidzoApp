01b08112628e4d93a07df921da11701d4f14ca7d
Mohamed Mabrouk <mohamedmbrouk123@gmail.com>
2023-05-17 10:20:17 +0300

    done

---
+++ KidzoApp/Components/Session/Session.jsx
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  Modal,
+  ActivityIndicator
+} from "react-native";
+import { useNavigation } from '@react-navigation/native';
+export default function Session() {
+    const navigation = useNavigation();
+  return (
+    <View>
+      <Text>Session</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
