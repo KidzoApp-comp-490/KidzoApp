5768e86a014aa751ebeb40bf175d0b101c2b4b73
AzizGerges <201927170@std.sci.cu.edu.eg>
2023-05-21 22:41:05 +0300

    edit

--- KidzoApp/Components/Doctors/MessageItem.jsx
+++ KidzoApp/Components/Doctors/MessageItem.jsx
@@ -5,18 +5,28 @@ import {
   Image,
   TouchableOpacity,
   ScrollView,
-  Modal,
 } from "react-native";
 import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import Settings from "../../assets/Settings/Sittings.png";
-import Ok from "../../assets/MedicalH/ok.png";
+import LogOut from "../../assets/Settings/LogOut.png";
 import { SignOut } from "../../db/firebase/auth";
 import Messages from "./Messages";
-import { getusersInfo } from "../../db/firebase/users";
-
+import { getusersInfo, getUserById } from "../../db/firebase/users";
+import { getUserUId } from "../../db/firebase/auth";
+import {
+  getMsgsforChatId,
+  getChat,
+  getMessage,
+  addMessage,
+} from "../../db/Chat";
 export default function MessageItem({ navigation }) {
   const [usersList, setUsersList] = useState([]);
+  const [messages2, setMessages2] = useState([]);
+  const [userID, setuserID] = useState("");
+  var userForDoc = new Set(
+    messages2.map((e) => (e.reciverUid == userID ? e.senderUid : null))
+  );
+  var usersDocState = Array.from(userForDoc);
   const getUsersList = async () => {
     const users = await getusersInfo();
     setUsersList(users);
@@ -28,9 +38,7 @@ export default function MessageItem({ navigation }) {
     // console.log(msgs)
   };
   React.useEffect(() => {
-    subscribe(() => {
-      getUsersList();
-    });
+    getUsersList();
   }, []);
   React.useEffect(() => {
     userForDoc.forEach((val) => {
@@ -68,17 +76,23 @@ export default function MessageItem({ navigation }) {
         <View style={styles.lineView}>
           <Text style={styles.line}>──────────────────────────────────</Text>
         </View>
-        {usersList.map((e, index) =>
-          e.job == "user" && e.uid != "ZF60Ucd4DVd66crcR4GO7yGSL8h1" ? (
-            <Messages
-              text1={e.fName}
-              text2={e.lName}
-              iconSrc={e.image}
-              docId={e.id}
-              key={index}
-            />
-          ) : null
+
+        {usersDocState.map((e) =>
+          usersList.map((u, index) =>
+            u.uid == e &&
+            u.job == "user" &&
+            u.uid != "ZF60Ucd4DVd66crcR4GO7yGSL8h1" ? (
+              <Messages
+                text1={u.fName}
+                text2={u.lName}
+                iconSrc={u.image}
+                docId={u.id}
+                key={index}
+              />
+            ) : null
+          )
         )}
+
         <View style={{ marginBottom: 50 }}></View>
       </ScrollView>
       <StatusBar style="auto" />
@@ -87,67 +101,15 @@ export default function MessageItem({ navigation }) {
 }
 const styles = StyleSheet.create({
   body: {
+    alignItems: "center",
     flex: 1,
     backgroundColor: "#ffff",
-  },
-  centeredView: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    marginTop: 22,
-  },
-  modalView: {
-    margin: 20,
-    backgroundColor: "white",
-    borderRadius: 20,
-    padding: 35,
-    alignItems: "center",
-    justifyContent: "center",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 4,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5,
-    width: 330,
-    height: 300,
-  },
-  content: {
-    width: 300,
-    height: 48,
-    backgroundColor: "#FFA8C5",
-    alignItems: "center",
-    borderRadius: 5,
-    marginHorizontal: 16,
-    justifyContent: "center",
-    marginBottom: 48,
-  },
-  text1: {
-    textAlign: "center",
-    color: "white",
-    fontFamily: "Montserrat",
-    fontWeight: 700,
-    fontSize: 16,
-  },
-  imageOk: {
-    width: 48,
-    height: 48,
   },
   titleView: {
     flexDirection: "row",
     alignItems: "center",
     marginTop: 75,
     marginLeft: 21.37,
-  },
-  title: {
-    color: "#0B3B63",
-    fontFamily: "Montserrat",
-    fontWeight: 700,
-    marginLeft: 21.37,
-    fontSize: 18,
-    marginRight: 200,
   },
   LogOutView: {
     flex: 1,
@@ -157,6 +119,13 @@ const styles = StyleSheet.create({
   LogOutImage: {
     width: 25,
     height: 25,
+  },
+  title: {
+    color: "#0B3B63",
+    fontFamily: "Montserrat",
+    fontWeight: 700,
+    marginLeft: 21.37,
+    fontSize: 18,
   },
   lineView: {
     marginTop: 32,
