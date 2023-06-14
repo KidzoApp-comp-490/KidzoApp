c597d9d1b1574860cb2a8308549933393326e7de
Mohamed Mabrouk <mohamedmbrouk123@gmail.com>
2023-06-14 22:59:32 +0300

    add section and and result

--- KidzoApp/Components/Session/ImageUploader.jsx
+++ KidzoApp/Components/Session/ImageUploader.jsx
@@ -6,12 +6,17 @@ import {
   Image,
   ScrollView,
   TouchableOpacity,
+
 } from "react-native";
+import { useState } from "react";
 import axios from "axios";
 import Frame from "../../assets/MedicalH/Frame.png";
-const BackendURL = "http://localhost:8090/detection";
+const BackendURL = "http://localhost:8090/detection"
+const BackendURL2 = "http://localhost:8090/file";
 
 export default function ImageUploader({ navigation }) {
+  const [fileContent, setFileContent] = useState('');
+  const [fileContentupdate, setFileContentupdate] = useState('');
   const runDetection = async () => {
     try {
       const response = await axios.post(BackendURL, {
@@ -24,6 +29,73 @@ export default function ImageUploader({ navigation }) {
       console.error(error);
     }
   };
+  // axios.get(BackendURL2)
+  // .then(response => {
+  //   const fileContent = response.data;
+  //   console.log(fileContent); // You can do further processing with the file content here
+  //   console.log("syaed")
+  // })
+  // .catch(error => {
+  //   console.error(error);
+  // });
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(BackendURL2);
+        setFileContent(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    const interval = setInterval(() => {
+      fetchData();
+    }, 200);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  useEffect(() => {
+    // Function to decrypt the file content
+    const decryptFileContent = () => {
+      const decryptedText = decryption(fileContent, 3);
+      setFileContentupdate(decryptedText);
+    };
+
+    // Call the decryptFileContent function
+    decryptFileContent();
+
+  }, []); // Empty dependency array to run the effect only once
+
+  function decryption(text, shift) {
+    let encrypt = false;
+    let result = "";
+
+    if (!encrypt) {
+      shift = -shift; // Invert the shift for decryption
+    }
+
+    for (let i = 0; i < text.length; i++) {
+      let ch = text.charAt(i);
+
+      if (ch.match(/[a-zA-Z]/)) {
+        let asciiOffset = ch === ch.toUpperCase() ? 65 : 97; // Determine ASCII offset based on uppercase or lowercase
+        let shifted = (ch.charCodeAt(0) - asciiOffset + shift + 26) % 26; // Apply shift and wrap around the alphabet
+        result += String.fromCharCode(shifted + asciiOffset);
+      } else {
+        result += ch; // Preserve non-alphabetic characters
+      }
+    }
+
+    return result;
+  }
+
+
+
 
   return (
     <View style={styles.body}>
@@ -58,33 +130,37 @@ export default function ImageUploader({ navigation }) {
 
       {/* button for 3 minites */}
 
-      <TouchableOpacity>
+      <TouchableOpacity
+        onPress={runDetection}
+      >
         <View style={styles.content}>
-          <Text style={styles.text1}>A session for 3 minutes</Text>
+          <Text style={styles.text1}>A session with no time limit</Text>
         </View>
       </TouchableOpacity>
 
       {/* button for 5 minites */}
 
-      <TouchableOpacity>
+      {/* <TouchableOpacity>
         <View style={styles.content2}>
           <Text style={styles.text1}>A session for 5 minutes</Text>
         </View>
-      </TouchableOpacity>
+      </TouchableOpacity> */}
 
       {/* button for an hour */}
 
-      <TouchableOpacity>
+      {/* <TouchableOpacity>
         <View style={styles.content2}>
           <Text style={styles.text1}>A session for an hour</Text>
         </View>
-      </TouchableOpacity>
+      </TouchableOpacity> */}
 
       {/* button for no limit time */}
-
+      <Text>the result for testing</Text>
       <TouchableOpacity>
         <View style={styles.content2}>
-          <Text style={styles.text1}>A session with no time limit</Text>
+          {/* <Text style={styles.text1}>A session with no time limit</Text> */}
+          <Text>{fileContent}</Text>
+
         </View>
       </TouchableOpacity>
     </View>
